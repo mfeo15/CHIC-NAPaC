19c0ff760bd2aa46bcb211f208768a013e27b0c8
Matteo Yann Feo <mfeo15@gmail.com>
2018-05-31 00:37:47 +0200

    Animate it !

--- Software/Android/Toygether/app/src/main/java/ch/epfl/chic/napac/toygether/toygether/WaitingForToyActivity.kt
+++ Software/Android/Toygether/app/src/main/java/ch/epfl/chic/napac/toygether/toygether/WaitingForToyActivity.kt
@@ -4,8 +4,6 @@ import android.animation.ValueAnimator
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.animation.LinearInterpolator
 import ch.epfl.chic.napac.toygether.toygether.connection.Client
 import ch.epfl.chic.napac.toygether.toygether.connection.Message
 import kotlinx.android.synthetic.main.activity_waiting_for_toy.*
@@ -13,6 +11,8 @@ import java.util.*
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
+import android.os.Handler
+import android.widget.ImageView
 
 
 class WaitingForToyActivity : AppCompatActivity(), Observer {
@@ -29,48 +29,12 @@ class WaitingForToyActivity : AppCompatActivity(), Observer {
         }
     }
 
+    fun withDelay(delay : Long, block : () -> Unit) {
+        Handler().postDelayed(Runnable(block), delay)
+    }
+
     override fun onResume() {
         super.onResume()
-
-        // 1
-        val valueAnimatorRight = ValueAnimator.ofFloat(imageView3.x, (imageView3.x+50))
-
-        //2
-        valueAnimatorRight.addUpdateListener {
-            val value = it.animatedValue as Float
-            imageView3.translationX = value
-        }
-
-
-        val valueAnimatorLeft = ValueAnimator.ofFloat((imageView3.x+50), imageView3.x)
-
-        //2
-        valueAnimatorLeft.addUpdateListener {
-            val value = it.animatedValue as Float
-            imageView3.translationX = value
-        }
-
-// 4
-        val animatorSet = AnimatorSet()
-// 5
-        animatorSet.play(valueAnimatorRight).after(valueAnimatorLeft)
-
-        animatorSet.addListener(object : AnimatorListenerAdapter() {
-
-            override fun onAnimationEnd(animation: Animator) {
-                super.onAnimationEnd(animation)
-                animatorSet.start()
-            }
-
-        })
-
-// 6
-
-        animatorSet.duration = 1000
-        animatorSet.start()
-
-
-
 
         Client.getInstance().addNewObserver(this)
     }
@@ -85,6 +49,11 @@ class WaitingForToyActivity : AppCompatActivity(), Observer {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_waiting_for_toy)
 
+        val x = imageView_red.x
+        animateDot(imageView_red, x)
+        withDelay(500) { animateDot(imageView_blue, x) }
+        withDelay(1000) { animateDot(imageView_yellow, x) }
+
         button_waiting_toy_close.setOnClickListener {
             finish()
         }
@@ -93,20 +62,20 @@ class WaitingForToyActivity : AppCompatActivity(), Observer {
         Client.getInstance().send( Message(toyCode, "U123", "2001").toString() )
     }
 
-    fun animateDot() {
+    fun animateDot(v: ImageView?, x : Float) {
 
         // It's just a jump to the right ...
-        val valueAnimatorRight = ValueAnimator.ofFloat(imageView3.x, (imageView3.x+50))
+        val valueAnimatorRight = ValueAnimator.ofFloat(x, (x+100))
         valueAnimatorRight.addUpdateListener {
             val value = it.animatedValue as Float
-            imageView3.translationX = value
+            v!!.translationX = value
         }
 
         // And one to thhe leeeeeeft
-        val valueAnimatorLeft = ValueAnimator.ofFloat((imageView3.x+50), imageView3.x)
+        val valueAnimatorLeft = ValueAnimator.ofFloat((x+100), x)
         valueAnimatorLeft.addUpdateListener {
             val value = it.animatedValue as Float
-            imageView3.translationX = value
+            v!!.translationX = value
         }
 
 
